42e78fcf22d3f7f275f3064abff14b8f478e3504
unknown <r.ichita.sys22@morijobi.ac.jp>
2022-11-08 12:54:44 +0900

    ëèðC³

--- Main.java
+++ Main.java
@@ -6,7 +6,13 @@ public class Main{
         double weight =sc.nextDouble();
         double bmi =weight / (height*height);
 
-	System.out.println(Math.round(bmi*100)/100.0);
+        System.out.println(Math.round(bmi*100)/100.0);
+	
+	double height2 =sc.nextDouble();
+	double weight2 =sc.nextDouble();
+	double bmi2 =weight2 / (height2*height2);
+
+	System.out.println(Math.round(bmi2*100)/100.0);
         
 
 
